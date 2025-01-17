7a31cff19cc8367f1c0a647657ed173b3aa26c43
Onkar Malawade <onkarmalawade@gmail.com>
2025-01-17 23:08:23 +0530

    user sign up page created look the structure : index.js > routes/ auth.routes.js > controller/auth.controller.js > user.model.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-
+import { generateToken } from "../lib/utils.js";
 export const signup = async (req, res) => {
     const{email, fullName, password} = req.body;
     try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({
+                message : "All fields are required"
+            })
+        }
         // hash password
         if (password.length < 8) {
             return res.status(400).json({
@@ -30,10 +35,22 @@ export const signup = async (req, res) => {
 
         if(newUser){
             // generate token
-            
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id : newUser._id,
+                email : newUser.email,
+                fullName : newUser.fullName,
+                profilePicture : newUser.profilePicture,
+            })
+        }else{
+            res.status(400).json({
+                message : "User not created"
+            });
         }
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
+        res.status(500).json({ message: "Internal Server error" });
     }
 };
 
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,5 +5,12 @@ export const generateToken = (userId, res) => {
         expiresIn: "7d",
     });
     // time allocate
-    res
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, //miliseconds
+        httpOnly: true, //can only be accessed by the server
+        sameSite: "strict", //cookie can only be sent to the same site
+        secure: process.env.NODE_ENV !== "development",
+    });
+
+    return token;
 }
